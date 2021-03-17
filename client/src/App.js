42ad310f6c4292fe162cbb67c682e15300f5b8fd
Richard Gabaree <richard.gabaree@gmail.com>
2021-03-17 12:04:50 -0500

    style update

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Login from "./components/login.js";
 import Signup from "./components/signup.js";
+import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
     return (
         <div>
-
+            <Login />
+            <Signup />
             {/* <Header />
             <Card />
             <Section /> */}
