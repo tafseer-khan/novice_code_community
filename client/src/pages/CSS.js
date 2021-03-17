ece1366441fe8769149e9d77fcb8bb3b285595ec
Dan Bushong <bushongdan@gmail.com>
2021-03-17 12:02:24 -0500

    navbar and pages added

---
+++ client/src/pages/CSS.js
@@ -0,0 +1,17 @@
+import React from "react";
+import NavbarConstant from "../components/NavbarConstant";
+
+
+
+function CSS(){
+
+    return(
+
+        <div>
+            <NavbarConstant />
+            <h1>The page is working</h1>
+            
+        </div>
+    )
+
+} export default CSS
