adac3ee9ec69d59d4e922cbb30fe55cc6e0b621b
Richard Gabaree <richard.gabaree@gmail.com>
2021-03-22 14:29:17 -0500

    hardcoded resources for HTML

--- client/src/pages/HTML.js
+++ client/src/pages/HTML.js
@@ -12,7 +12,22 @@ function HTML(){
             
             <div style={{float: "left" , width: "50%"}}>
 
-            <h1>The page is working</h1>
+                <h2>HTML Resources</h2>
+                <br></br>
+                <h5>What is HTML?</h5>
+                    <p>HTML — Hypertext Markup Language — is the language used for creating web pages and other web-based documents. It consists mainly of matching pairs of angle-bracketed tags, enclosing sections of human-meaningful text.
+
+                <br></br>
+                <br></br>
+                <h5>Websites</h5>
+                <p> 
+                    <a href="https://www.w3schools.com/html/">HTML Tutorial</a>
+                </p>
+                <p>
+                    <a href="https://www.codecademy.com/learn/learn-html">Coding Academy</a>
+                </p>
+</p>
+
 
             </div >
 
