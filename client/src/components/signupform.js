d266f35d516a3d73d963a1307434390dfff3617c
Daniel <daniel.castelline@gmail.com>
2021-03-21 21:47:26 -0500

    required usestate, axios, passport in forms, added states and values to form components

--- client/src/components/signupform.js
+++ client/src/components/signupform.js
@@ -1,37 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+const passport = require('passport-local');
+const axios = require('axios');
 
 function SignupForm() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     return (
         <div className="form-group">
             <label className="username-label">Username:</label>
             <br />
-
-            <input type="username"
+            <input
+                type="username"
+                name="username"
                 className="form-control"
                 placeholder="Choose a username"
-
+                value={username}
+                onChange={setUsername}
             />
             <br />
             <label className="email-label">Email:</label>
             <br />
-
-            <input type="email"
+            <input
+                name="email"
+                type="email"
                 className="form-control"
                 placeholder="Enter your email"
-
+                value={email}
+                onChange={setEmail}
             />
             <br />
             <label className="password-label">Password:</label>
             <br />
-
-            <input type="password"
+            <input
+                name="password"
+                type="password"
                 className="form-control"
-
                 placeholder="Enter a password"
+                value={password}
+                onChange={setPassword}
             />
             <br />
-            <button type="submit" className="signup-btn">Sign Up</button>
+            <button type="submit" className="signup-btn" onClick={handleSubmit}>Sign Up</button>
         </div>
         
     )
